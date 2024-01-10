df1d498810f388c597f73d2981eb3ac9b9dcc85d
akifberabasar <akifbera583@gmail.com>
2024-01-10 18:33:42 +0300

    Proje oluşturuldu

---
+++ FinalOdeviServis/src/main/java/tr/edu/medipol/yazilimgelistirme/FinalOdeviServis/FinalOdeviServisApplication.java
@@ -0,0 +1,13 @@
+package tr.edu.medipol.yazilimgelistirme.FinalOdeviServis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalOdeviServisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalOdeviServisApplication.class, args);
+	}
+
+}
---
+++ FinalOdeviServis/src/test/java/tr/edu/medipol/yazilimgelistirme/FinalOdeviServis/FinalOdeviServisApplicationTests.java
@@ -0,0 +1,13 @@
+package tr.edu.medipol.yazilimgelistirme.FinalOdeviServis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalOdeviServisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
